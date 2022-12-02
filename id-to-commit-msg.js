cd94a6ea773a2e7d3d2d46e6e91f66f0b4608624
Max Greive <max.greive@latori.com>
2022-12-02 09:08:57 +0100

    husky commit messages

---
+++ id-to-commit-msg.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+import * as fs from 'fs';
+
+const JIRA_TAG = 'TAG';
+const TRELLO_URL = 'https://trello.com/c/';
+
+/* If message title:
+ * * Doesn't start with square brackets []
+ * * Doesn't start with Merge branch
+ * * Doesn't start with Merge pull request
+ * * Doesn't start with #
+ * and
+ * branch name starts with ${JIRA_TAG}-XXX (e.g. TAG-123-branch-description)
+ * then prepend the issue tag to the commit message
+ *
+ * My awesome commit -> [TAG-123] My awesome commit
+ */
+
+const startsWithBraces = (str) => str.match(/^\[[^\]]/);
+const startsWithMergeBranch = (str) => str.indexOf('Merge branch') === 0;
+const startsWithMergePR = (str) => str.indexOf('Merge pull request') === 0;
+const startsWithHash = (str) => str.indexOf('#') === 0;
+const isInvalidMessage = (str) =>
+  !startsWithBraces(str) && !startsWithMergeBranch(str) && !startsWithMergePR(str) && !startsWithHash(str);
+const tagMatcher = new RegExp(`\/([a-zA-Z0-9]+)---`, 'i');
+const getIssueTagFromBranchName = (str) => {
+  const matched = str.match(tagMatcher);
+  return matched && matched[1];
+};
+
+const messageFile = process.env.GIT_PARAMS;
+const message = fs.readFileSync(messageFile, { encoding: 'utf-8' });
+const messageTitle = message.split('\n')[0];
+const branchName = require('child_process').execSync('git rev-parse --abbrev-ref HEAD', { encoding: 'utf-8' }).split('\n')[0];
+const issueTag = getIssueTagFromBranchName(branchName);
+
+if (issueTag && isInvalidMessage(messageTitle)) {
+  // Apply the issue tag to message title
+  const messageLines = message.split('\n');
+  messageLines[0] = `[${issueTag}] ${messageTitle}\n${TRELLO_URL}${issueTag}`;
+  // fs.writeFileSync(messageFile, messageLines.join('\n'), { encoding: 'utf-8' });
+  console.log(`New message title: ${messageLines[0]}`);
+}
